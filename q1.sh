ce080338531c89480f7aa8236cd795c8af93164a
arpan-maity <arpan.maity@niser.ac.in.com>
2019-08-09 21:10:41 +0530

    commitsweek1

---
+++ q1.sh
@@ -0,0 +1,6 @@
+echo "Enter Name :"
+read name
+echo "hello $name"
+mkdir $name
+touch $name/"$name".cpp
+echo "Your folder has been created."
---
+++ q2.sh
@@ -0,0 +1,10 @@
+echo "Enter your name: "
+read name
+mkdir $name
+cd $name
+touch $name/"$name"1.cpp
+touch $name/"$name"2.cpp
+touch $name/"$name"3.cpp
+touch $name/"$name"4.cpp
+touch $name/"$name"5.cpp
+echo "All your folders have been created."
